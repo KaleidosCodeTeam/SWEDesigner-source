b4bc133436404b44c7dd948dae5cfc97c8b545f4
Matteo Sovilla <teo.sovi@gmail.com>
2017-06-05 10:01:51 +0200

    Aggiunto pulsante test per cambio panel inizio lavoro panel link

---
+++ panel-connect.js
@@ -0,0 +1,14 @@
+/**
+ * Created by socs on 05/06/2017.
+ */
+$(".testbutton-panel").click(function(){
+    $(".panel-swedesigner").css("visibility","hidden")
+});
+
+$("#showpanel-zero").click(function(){
+    $("#panel-zero").css("visibility","visible")
+});
+
+$("#showpanel-link").click(function(){
+    $("#panel-link").css("visibility","visible")
+});
---
+++ panel-link.js
@@ -0,0 +1,3 @@
+/**
+ * Created by socs on 05/06/2017.
+ */
