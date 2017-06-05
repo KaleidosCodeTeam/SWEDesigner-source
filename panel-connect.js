52f025040775cfb08e5e211e1cf49e29f6ba786a
Matteo Sovilla <teo.sovi@gmail.com>
2017-06-05 11:14:35 +0200

    Aggiunti pannelli link comment e package

--- panel-connect.js
+++ panel-connect.js
@@ -1,14 +1,24 @@
 /**
  * Created by socs on 05/06/2017.
  */
+$("#panel-zero").css("display","inline");
+
 $(".testbutton-panel").click(function(){
-    $(".panel-swedesigner").css("visibility","hidden")
+    $(".panel-swedesigner").css("display","none")
 });
 
 $("#showpanel-zero").click(function(){
-    $("#panel-zero").css("visibility","visible")
+    $("#panel-zero").css("display","inline")
 });
 
 $("#showpanel-link").click(function(){
-    $("#panel-link").css("visibility","visible")
+    $("#panel-link").css("display","inline")
+});
+
+$("#showpanel-comment").click(function(){
+    $("#panel-comment").css("display","inline")
+});
+
+$("#showpanel-package").click(function(){
+    $("#panel-package").css("display","inline")
 });
