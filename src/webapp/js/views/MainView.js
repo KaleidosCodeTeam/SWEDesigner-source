a7801508f57e0091394c70f6c119e75ce57c8d37
Unknown <franci.pezzuto@gmail.com>
2017-06-09 21:41:13 +0200

    Adattamento grezzo per uso di Requirejs del file MainView.js
    (da applicare anche agli altri file views e models)

--- src/webapp/js/views/MainView.js
+++ src/webapp/js/views/MainView.js
@@ -1,15 +1,25 @@
-var MainView = Backbone.View.extend({
-	el: 'body',
-	events: {},
-	initialize: function() {
-		titleBarView = new TitleBarView;
-		toolbarView = new ToolbarView;
-		addressView = new AddressView;
-		editPanelView = new EditPanelView;
-		paper = new joint.dia.Paper;
-	},
-	render: function() {
-	}
-});
+/** Per Requirejs (da applicare a tutti i file che si creeranno) */
+define ([
+	'jquery',
+	'underscore',
+	'backbone',
+	'joint',
+	/** ecc. */
+], function ($, _, Backbone, joint) {
+	var MainView = Backbone.View.extend({
+		el: 'body',
+		events: {},
+		initialize: function() {
+			titleBarView = new TitleBarView;
+			toolbarView = new ToolbarView;
+			addressView = new AddressView;
+			editPanelView = new EditPanelView;
+			paper = new joint.dia.Paper;
+		},
+		render: function() {
+		}
+	});
 
-var mainView = new MainView();
+	var mainView = new MainView();
+	// BISOGNERÀ FARE return MainView;
+});
