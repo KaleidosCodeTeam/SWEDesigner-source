81409f21382d34edffdbd41bd8011d16c2c45929
mbonolo <bonolomarco@gmail.com>
2017-06-13 10:57:06 +0200

    + PackageItems.js

--- src/app/js/models/items/PackageItems.js
+++ src/app/js/models/items/PackageItems.js
@@ -1,42 +1,33 @@
+Swedesigner.model.packageDiagram.items = {};
 Swedesigner.model.packageDiagram.items.Package = joint.shapes.basic.Generic.extend({
-    markup: ['<g class="rotatable">',
-    		'<g class="scalable">',
-    		'<rect class="package-name-rect"/><rect class="package-divider-rect"/>',
-    		'</g>',
-    		'<text class="package-name-text"/>',
-    		'</g>'
-    ].join(),
+    markup: [
+        '<g class="scalable">',
+        '<rect class="uml-package-name-rect"/>',
+        '</g>',
+        '<text class="uml-package-name-text"/>',
+    ].join(''),
     defaults: _.defaultsDeep({
-        type: 'package.Package',
-        position: {x: 200, y: 200},
-        size: {width: 100, height: 100},
+        type: 'uml.Package',
         attrs: {
             rect: {'width': 200},
-            '.package-name-rect': {
-                'stroke': '#d6b656',
-                'stroke-width': 1,
-                'fill': '#fff2cc'
+            '.uml-package-name-rect': {
+                'fill': '#55dd77',
+                'stroke': '#000',
+                'stroke-width': 2
             },
-            '.package-divider-rect': {
-                'stroke': 'black',
-                'stroke-width': 1,
-                'fill': 'black'
-            },
-            '.package-name-text': {
-                'ref': '.uml-class-name-rect',
-                'ref-y': .6,
-                'ref-x': .5,
-                'text-anchor': 'middle',
-                'y-alignment': 'middle',
-                'fill': '#222222',
-                'font-size': 16,
-                'font-family': 'Roboto'
+            '.uml-package-name-text': {
+            	'ref': '.uml-package-name-rect',
+            	'ref-y': .5,
+            	'ref-x': .5,
+            	'text-anchor': 'middle',
+            	'y-alignment': 'middle',
+            	'font-weight': 'bold',
+            	'fill': 'black',
+            	'font-size': 12,
+            	'font-family': 'Times New Roman'
             }
         },
-        values: {
-            name: "PackageName",
-          	subPackages = []
-        }
+        name: ['PackageName']
     }, joint.shapes.basic.Generic.prototype.defaults),
     initialize: function() {
         this.on('change:name', function() {
@@ -58,25 +49,20 @@ Swedesigner.model.packageDiagram.items.Package = joint.shapes.basic.Generic.exte
         _.each(rects, function(rect) {
             var lines = _.isArray(rect.text) ? rect.text : [rect.text];
             var rectHeight = lines.length * 20 + 20;
-            attrs['.package-' + rect.type + '-text'].text = lines.join('\n');
-            attrs['.uml-class-' + rect.type + '-rect'].height = rectHeight;
-            attrs['.uml-class-' + rect.type + '-rect'].transform = 'translate(0,' + offsetY + ')';
-
+            attrs['.uml-package-' + rect.type + '-text'].text = lines.join('\n');
+            attrs['.uml-package-' + rect.type + '-rect'].height = rectHeight;
+            attrs['.uml-package-' + rect.type + '-rect'].transform = 'translate(0,' + offsetY + ')';
             offsetY += rectHeight;
         });
     }
 });
 
 Swedesigner.model.packageDiagram.items.PackageView = joint.dia.ElementView.extend({
-	/*
     initialize: function() {
-
         joint.dia.ElementView.prototype.initialize.apply(this, arguments);
-
         this.listenTo(this.model, 'uml-update', function() {
             this.update();
             this.resize();
         });
     }
-    */
 });
