9af28681f625637c4d31017dc0037c2a07d3a4f5
Matteo Sovilla <teo.sovi@gmail.com>
2017-06-14 16:56:40 +0200

    Corrette parentesi

--- src/webapp/js/views/toolbarView.js
+++ src/webapp/js/views/toolbarView.js
@@ -15,16 +15,15 @@ define ([
 		render: function() {
 		},
 		getItems: function() {
-		}
+		},
 		addCell: function(event) {
 			type = event.currentTarget.id;
-				var p = new Swedesigner.model.packageDiagram.items.Package{{
+				var p = new Swedesigner.model.packageDiagram.items.Package({
 					position: { x:100  , y: 100 },
 					size: { width: 180, height: 50 },
 					_package: 'Prova'
-				})};
+				});
 				graph.addCell(p);
-				
 		}
-	)};
+	});
 });
