d29ead8ade1b1c24e8483837f2d0960cdc260046
mbonolo <bonolomarco@gmail.com>
2017-06-16 16:08:58 +0200

    Mod toolbarModel.js

--- src/webapp/js/models/toolbarModel.js
+++ src/webapp/js/models/toolbarModel.js
@@ -3,10 +3,29 @@ define ([
     'underscore',
     'backbone',
     'joint',
-], function ($, _, Backbone, joint) {
-	var toolbarModel = Backbone.Model.extend({
-		initialize: function() {
+    'js/models/items/swedesignerItems'
+], function ($, _, Backbone, joint, Swedesigner) {
+	var ToolbarModel = Backbone.Model.extend({
+		mainModel: {},
+		items: {},
+		initialize: function(options) {
+			this.mainModel = options.model;
+			this.createItems();
+		},
+		currentDiagram: function() {
+			return this.mainModel.project.currentGraph.getDiagramType();
+		},
+		createItems: function() {
+			for(obj in Swedesigner.model.packageDiagram.items)
+				this.items['packageDiagram-'+obj] = Swedesigner.model.packageDiagram.items[obj];
+			for(obj in Swedesigner.model.classDiagram.items)
+				this.items['classDiagram-'+obj] = Swedesigner.model.classDiagram.items[obj];
+		},
+		addElement: function (id) {
+			console.log(id);
+			var el = new this.items[id];
+			this.mainModel.project.currentGraph.graph.addCell(el);
 		}
 	});
-	return toolbarModel;
+	return ToolbarModel;
 });
