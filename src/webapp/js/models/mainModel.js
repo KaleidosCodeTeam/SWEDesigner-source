f84883db4652ebc7e63352698eadf35026bbabfc
mbonolo <bonolomarco@gmail.com>
2017-06-16 16:09:19 +0200

    Mod mainModel.js

--- src/webapp/js/models/mainModel.js
+++ src/webapp/js/models/mainModel.js
@@ -6,17 +6,19 @@ define ([
     'js/models/items/swedesignerItems'
 
     //'js/models/project'
-], function ($, _, Backbone, joint, items/*, Project*/) {
+], function ($, _, Backbone, joint, Swedesigner/*, Project*/) {
 	var MainModel = Backbone.Model.extend({
-		graph: {},
 		//urlRoot: '/path(forse)',
 		project: {
 			projectPkgDiagram: {},
             currentGraph: {},
 		},
 		initialize: function() {
-			this.project.projectPkgDiagram = new items.model.Diagram('packageDiagram');
-			this.currentGraph = this.project.projectPkgDiagram;
+			console.log("MainModel initialized");
+			this.project.projectPkgDiagram = new Swedesigner.model.Diagram('packageDiagram');
+			console.log('Project.projectPkgDiagram ' + this.project.projectPkgDiagram);
+			this.project.currentGraph = this.project.projectPkgDiagram;
+			console.log('Project.currentGraph ' + this.project.currentGraph);
 			this.graph = new joint.dia.Graph();
 		},
 		/*saveProject: function() {
