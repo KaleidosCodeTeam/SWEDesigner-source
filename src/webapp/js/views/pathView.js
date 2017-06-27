9fd196e1097675123f9b053f350028638989ebf0
francescopezzuto <franci.pezzuto@gmail.com>
2017-06-27 19:26:10 +0200

    Sistemato path
    Rimosso model e ri-adattata view

--- src/webapp/js/views/pathView.js
+++ src/webapp/js/views/pathView.js
@@ -4,10 +4,9 @@ define ([
 	'backbone',
 	'joint',
 	'js/views/projectView',
-	'js/models/projectModel',
-	'js/models/pathModel'
+	'js/models/projectModel'
 	/** ecc. */
-], function ($, _, Backbone, joint, projectView, projectModel, pathModel) {
+], function ($, _, Backbone, joint, projectView, projectModel) {
 	var PathView = Backbone.View.extend({
 		el: $('.breadcrumb'),
 		parent: {},
@@ -15,7 +14,6 @@ define ([
 			'click .switchDiagram': 'switchDiagram'
 		},
 		initialize: function() {
-			this.model = pathModel;
 			this.listenTo(projectModel,'switchgraph',this.render);
 			this.render();
 		},
@@ -23,28 +21,27 @@ define ([
 			this.renderPath();
 		},
 		renderPath: function() {
-			var currentDiagram = this.model.currentDiagram();
-			var diagrams = {'packageDiagram': 1, 'classDiagram': 2, 'bubbleFlowchart': 3};
+			var currentDiagram = projectModel.currentDiagramType;
+			var diagrams = {'packageDiagram': 1, 'classDiagram': 2, 'bubbleDiagram': 3};
 			this.$el.empty();
 			if(diagrams[currentDiagram] > diagrams['packageDiagram'])
-				this.$el.append('<li id="packageDiagram" class="switchDiagram"><a href=#">Package</a></li>');
+				this.$el.append('<li id="packageDiagram" class="switchDiagram"><a href="#">Package</a></li>');
 			else
 				if(diagrams[currentDiagram] == diagrams['packageDiagram'])
 					this.$el.append('<li id="packageDiagram" class="active">Package</li>');
 			if(diagrams[currentDiagram] > diagrams['classDiagram'])
-				this.$el.append('<li id="classDiagram" class="switchDiagram"><a href=#">Class</a></li>');
+				this.$el.append('<li id="classDiagram" class="switchDiagram"><a href="#">Class</a></li>');
 			else
 				if(diagrams[currentDiagram] == diagrams['classDiagram'])
 					this.$el.append('<li id="classDiagram" class="active">Class</li>');
-			if(diagrams[currentDiagram] > diagrams['bubbleFlowchart'])
-				this.$el.append('<li id="bubbleFlowchart" class="switchDiagram"><a href=#">Bubble</a></li>');
+			if(diagrams[currentDiagram] > diagrams['bubbleDiagram'])
+				this.$el.append('<li id="bubbleDiagram" class="switchDiagram"><a href="#">Bubble</a></li>');
 			else
-				if(diagrams[currentDiagram] == diagrams['bubbleFlowchart'])
-					this.$el.append('<li id="bubbleFlowchart" class="active">Bubble</li>');
+				if(diagrams[currentDiagram] == diagrams['bubbleDiagram'])
+					this.$el.append('<li id="bubbleDiagram" class="active">Bubble</li>');
 		},
 		switchDiagram: function(event) {
 			projectView.switchOut(event.currentTarget.id);
-			//this.model.switchDiagram(event.currentTarget.id);
 		}
 	});
 	return new PathView;
