d59db877ca5e26b36681f1ae11a94d915615fe0d
Matteo Sovilla <teo.sovi@gmail.com>
2017-06-29 16:20:32 +0200

    Sistemato css

--- src/webapp/js/models/projectModel.js
+++ src/webapp/js/models/projectModel.js
@@ -17,6 +17,67 @@ define ([
             this.currentDiagramType = 'packageDiagram';
             let myAdjustVertices = _.partial(this.adjustVertices, this.graph);
             this.graph.on('add remove change:source change:target', myAdjustVertices);
+            /*/////////////////////////////////////////////////////////////////////////////////////////////////////////// Ridimensionamento del genitore in base ai figli
+            this.graph.on('change:size', function(cell, newPosition, opt) {
+
+                if (opt.skipParentHandler) return;
+
+                if (cell.get('embeds') && cell.get('embeds').length) {
+                    // If we're manipulating a parent element, let's store
+                    // it's original size to a special property so that
+                    // we can shrink the parent element back while manipulating
+                    // its children.
+                    cell.set('originalSize', cell.get('size'));
+                }
+            });
+            this.graph.on('change:position', function(cell, newPosition, opt) {
+
+                if (opt.skipParentHandler) return;
+
+                if (cell.get('embeds') && cell.get('embeds').length) {
+                    // If we're manipulating a parent element, let's store
+                    // it's original position to a special property so that
+                    // we can shrink the parent element back while manipulating
+                    // its children.
+                    cell.set('originalPosition', cell.get('position'));
+                }
+
+                var parentId = cell.get('parent');
+                if (!parentId) return;
+
+                var parent = graph.getCell(parentId);
+                var parentBbox = parent.getBBox();
+
+                if (!parent.get('originalPosition')) parent.set('originalPosition', parent.get('position'));
+                if (!parent.get('originalSize')) parent.set('originalSize', parent.get('size'));
+
+                var originalPosition = parent.get('originalPosition');
+                var originalSize = parent.get('originalSize');
+
+                var newX = originalPosition.x;
+                var newY = originalPosition.y;
+                var newCornerX = originalPosition.x + originalSize.width;
+                var newCornerY = originalPosition.y + originalSize.height;
+
+                _.each(parent.getEmbeddedCells(), function(child) {
+
+                    var childBbox = child.getBBox();
+
+                    if (childBbox.x < newX) { newX = childBbox.x; }
+                    if (childBbox.y < newY) { newY = childBbox.y; }
+                    if (childBbox.corner().x > newCornerX) { newCornerX = childBbox.corner().x; }
+                    if (childBbox.corner().y > newCornerY) { newCornerY = childBbox.corner().y; }
+                });
+
+                // Note that we also pass a flag so that we know we shouldn't adjust the
+                // `originalPosition` and `originalSize` in our handlers as a reaction
+                // on the following `set()` call.
+                parent.set({
+                    position: { x: newX, y: newY },
+                    size: { width: newCornerX - newX, height: newCornerY - newY }
+                }, { skipParentHandler: true });
+            });
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////*/// FINE RIDIMENSIONAMENTO
 		},
         /**
          *  @function projectModel#addItem
