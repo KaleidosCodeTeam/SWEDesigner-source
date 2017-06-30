30436c282a8b398523341b6b835b2c06208fe1df
pepe11 <sannagiovanni9@gmail.com>
2017-06-30 09:06:38 +0200

    aggiustamenti codeGenerator

---
+++ src/server/codeGenerator/PackageName_JavaCode/PackageName/NomeClasse3.java
@@ -0,0 +1,7 @@
+package PackageName;
+public class NomeClasse3 implements NomeInterfaccia1 
+ { 
+private static final x nuovoAttributo5 ;
+private int nuovoAttributo6  = 5;
+protected y nuovoAttributo7 ;
+};
---
+++ src/server/codeGenerator/PackageName_JavaCode/PackageName/NomeInterfaccia1.java
@@ -0,0 +1,6 @@
+package PackageName;
+public interface NomeInterfaccia1 
+ { 
+public int oper3(char nuovoParametro4, nuovoParametro); 
+public Object oper4( nuovoParametro); 
+};
